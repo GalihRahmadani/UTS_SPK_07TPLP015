306e559a70ac1876c25b4264d385d3570707cfc6
GalihRahmadani <138578432+GalihRahmadani@users.noreply.github.com>
2023-10-31 12:07:14 +0700

    Add files via upload

---
+++ uts_spk_galihrahmadani_201011401263.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.spk_uts ("Nama Helm",harga,berat,"Double Visor","Sertifikat Si","Garansi","Alternatif","Nilai Akhir","Range") VALUES
+	 ('BMC TOURING','100,000','1100 gram','Tidak ada','SNI',1,2.6956506,0.08560285,8),
+	 ('HIU ARROW','150,000','1100 gram','Ada','SNI',1,3.2530618,0.103303954,6),
+	 ('INK CX 22','300,000','1400 gram','Tidak ada','SNI DOT',4,3.5910337,0.11403656,2),
+	 ('MDS PROTECT OR','200,000','1300 gram','Tidak ada','SNI',2,3.3963068,0.10785283,4),
+	 ('KYT GALAXY SLIDE','350,000','1400 gram','Ada','SNI DOT',5,4.510683,0.14324087,1),
+	 ('INK Trooper','650,000','1300 gam','Ada','SNI',2,3.317442,0.1053484,5),
+	 ('NJS Freedom','650,000','1200 gram','Ada','SNI',3,3.4134073,0.108395875,3),
+	 ('JPX JP Signature','700,000','1200 gram','Ada','SNI',2,2.7046652,0.08588912,7),
+	 ('Zeus ZS816 C Retro','700,000','1200 gram','Tidak ada','SNI',2,2.2412217,0.07117204,10),
+	 ('HBC Cakil','400,000','1000 gram','Ada','SNI',1,2.3667245,0.0751575,9);
